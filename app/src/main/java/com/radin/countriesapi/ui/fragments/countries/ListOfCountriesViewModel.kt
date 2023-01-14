34b8837c19234923f639307b3d83dde87ac41fd1
RenHeyzer <vesolyyradin@gmail.com>
2023-01-14 15:31:30 +0600

    ViewModel for ListOfCountriesFragment

---
+++ app/src/main/java/com/radin/countriesapi/ui/fragments/countries/ListOfCountriesViewModel.kt
@@ -0,0 +1,8 @@
+package com.radin.countriesapi.ui.fragments.countries
+
+import com.radin.countriesapi.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListOfCountriesViewModel @Inject constructor() : BaseViewModel()
