11dc59e9dd33c8c463a12ec5526660dea123bb0b
RenHeyzer <vesolyyradin@gmail.com>
2023-01-14 23:14:55 +0600

    request for get all countries and get single country

--- app/src/main/java/com/radin/countriesapi/data/remote/api/CountriesApiService.kt
+++ app/src/main/java/com/radin/countriesapi/data/remote/api/CountriesApiService.kt
@@ -1,3 +1,17 @@
 package com.radin.countriesapi.data.remote.api
 
-interface CountriesApiService
+import com.radin.countriesapi.data.models.CountryDto
+import com.radin.countriesapi.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CountriesApiService {
+
+    @GET(Constants.ALL_ENDPOINT)
+    suspend fun getAllCountries(): List<CountryDto>
+
+    @GET(Constants.BY_NAME_ENDPOINT)
+    suspend fun getCountryDetail(
+        @Path("name") name: String
+    ): List<CountryDto>
+}
